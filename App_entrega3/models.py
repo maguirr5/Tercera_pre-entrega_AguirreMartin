3be343f7fd2b9842569f58bb3a143614a14ec886
Martin Aguirre <tinchocaminano@gmail.com>
2023-11-08 16:46:37 -0300

    agregado app y modelos

---
+++ App_entrega3/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    edad = models.IntegerField()
+    #def __str__(self):
+     #   return f"{self.nombre} {self.apellido} , {edad}"
+
+
+class ArteMarcial(models.Model):
+    estilo = models.CharField(max_length=255)
+    pais = models.CharField(max_length=255)
+    zona = models.CharField(max_length=128)
+    #def __str__(self):
+     #   return f"{self.estilo} {self.pais} , {zona}"
+
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    duracion = models.IntegerField()
+    #def __str__(self):
+     #   return f"{self.titulo} {self.director} , {duracion}"
